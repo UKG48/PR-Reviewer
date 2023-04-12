124775216ab35ba6867980d164f99fef7dfb0e48
Ayush A Gupta <ayush.a.gupta@ukg.com>
2023-04-12 23:29:05 +0530

    Corrected github variable type.

--- .github/workflows/pr_reviewer.py
+++ .github/workflows/pr_reviewer.py
@@ -9,7 +9,7 @@ ACCOUNT_GITHUB_TOKEN = os.getenv('ACCOUNT_GITHUB_TOKEN')
 REPO_NAME = os.getenv('REPO_NAME')
 REPO_OWNER = os.getenv('REPO_OWNER')
 
-#Reading secrets set in github.
+#Reading repository secrets set in github.
 SECRET_OPENAI_TOKEN = os.environ['SECRET_OPENAI_TOKEN']
 SECRET_GITHUB_KEY = os.environ['SECRET_GITHUB_KEY']
 
