d2f7dbc340c4f7fd5d30fc4b51fbc5e62afb6af0
Ayush A Gupta <ayush.a.gupta@ukg.com>
2023-04-13 15:54:37 +0530

    Fibonacci implementation in java

--- fibonacci.java
+++ fibonacci.java
@@ -1,17 +1,19 @@
 //This class is an implementation of fibonacci series.
 class FibonacciExample {
     public static void main(String args[]) {
+        try {
+            int p1 = 0, p2 = 1, p3, i, count = 10; // changed the variables
+            System.out.print(p1 + " " + p2);// printing 0 and 1
 
-        int p1 = 0, p2 = 1, p3, i, count = 10; // changed the variables
-        System.out.print(p1 + " " + p2);// printing 0 and 1
-
-        for (i = 2; i < count; ++i)// loop starts from 2 because 0 and 1 are already printed
-        {
-            p3 = p1 + p2;
-            System.out.print(" " + p3);
-            p1 = p2;
-            p2 = p3;
+            for (i = 2; i < count; ++i)// loop starts from 2 because 0 and 1 are already printed
+            {
+                p3 = p1 + p2;
+                System.out.print(" " + p3);
+                p1 = p2;
+                p2 = p3;
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
         }
-
     }
 }
